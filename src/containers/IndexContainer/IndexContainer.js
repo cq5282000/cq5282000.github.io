ddba94bdd2c107ac41fa44e5a961de8413075a35
chenqu <chenqu@meituan.com>
2017-11-25 16:57:33 +0800

    add

---
+++ src/containers/IndexContainer/IndexContainer.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chenqu on 2017/11/25.
+ */
+import React, { Component } from 'react';
+
+export default class IndexContainer extends Component {
+    render() {
+        return (
+            <div>
+                营养快线的个人主页
+            </div>
+        );
+    }
+}
---
+++ src/entry/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chenqu on 2017/9/4.
+ */
+import IndexDemoContainer from '../containers/IndexFetchDemo/IndexFetchDemoContainer';
+import render from '../libs/createRender';
+
+render(IndexDemoContainer);
+if (module.hot) {
+    module.hot.accept('../containers/IndexFetchDemo/IndexFetchDemoContainer', () => {
+        const NewEntry = require('../containers/IndexFetchDemo/IndexFetchDemoContainer').default;
+        render(NewEntry);
+    });
+}
